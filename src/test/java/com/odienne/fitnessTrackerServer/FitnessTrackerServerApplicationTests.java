62a77efd7eecab4524d021a452af27e04968be78
Adam Odienne <odienne.adam@gmail.com>
2024-09-23 15:33:07 +0200

    other - project related

---
+++ src/test/java/com/odienne/fitnessTrackerServer/FitnessTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odienne.fitnessTrackerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
